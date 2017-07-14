eb8c6f4e68663cf4a46e5d2d89d5455754799921
kaikun213 <kaktus.w.h@gmx.de>
2017-07-14 16:53:59 +0200

    Index (Recorder & Editor missing)

--- src/Debugger/index.js
+++ src/Debugger/index.js
@@ -1,8 +1,8 @@
 
 export Debugger from './Debugger';
 
-export Editor from './Editor'
-export Recorder from './Recorder';
+export {Editor} from './Editor'
+export {Recorder} from './Recorder';
 export StateDiff from './StateDiff';
 export TxTrace from './TxTrace';
-export VmTrace from './VmTrace';
+export { VmTrace, Instruction, Instructions, Slider, Stack, Memory, Storage, get_info } from './VmTrace';
