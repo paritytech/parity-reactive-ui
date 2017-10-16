5f5b99a21d43283e4800b0acda22ca3a7958d5ec
Arthur Stemmer <apstemmer@hotmail.com>
2017-10-16 15:30:07 +0100

    ESLint conform

--- src/CoinList.jsx
+++ src/CoinList.jsx
@@ -1,71 +1,76 @@
-import {ReactiveComponent} from 'oo7-react';
-import {bonds} from 'oo7-parity';
-import {Popup, Image} from 'semantic-ui-react';
+import { ReactiveComponent } from 'oo7-react';
+import { bonds } from 'oo7-parity';
+import { Popup, Image } from 'semantic-ui-react';
 import unknownIcon from './assets/unknown-64x64.png';
 import React from 'react';
 
-export class CoinList extends ReactiveComponent{
-  constructor(){
+export class CoinList extends ReactiveComponent {
+  constructor () {
     super(['tokens']);
   }
 
   style = {
-  	display: 'inline-block',
-    backgroundColor:'#e2e2e2',
-  	padding:'10px',
-  	borderRadius: '10px',
-  	overflow:'hidden'
+    display: 'inline-block',
+    backgroundColor: '#e2e2e2',
+    padding: '10px',
+    borderRadius: '10px',
+    overflow: 'hidden'
   }
 
-  render(){
-    let {tokens} = this.state;
-	   console.log('stati',this.state);
-    if(typeof tokens == "undefined" || tokens.length == 0) return <span>-</span>
-    return (<div style={this.style}>
-      {this.state.tokens.map((elem)=>{
-        return (<CoinIcon
-          key={elem.name}
-          src={bonds.githubhint.entries(elem.img).map((imgObj)=>{
+  render () {
+    let { tokens } = this.state;
 
-            console.log('imgobj',imgObj);
-            return imgObj[0]
-          })}
-          balance={elem.balance.toString()}
-          tla={elem.tla}
-          name={elem.name}
-	  />);
-      })}
+    console.log('stati', this.state);
+    if (typeof tokens === 'undefined' || tokens.length === 0) {
+      return <span>-</span>;
+    }
+    return (<div style={ this.style }>
+      { this.state.tokens.map((elem) => {
+        return (
+          <CoinIcon
+            key={ elem.name }
+            src={ bonds.githubhint.entries(elem.img).map((imgObj) => {
+              console.log('imgobj', imgObj);
+              return imgObj[0];
+            }) }
+            balance={ elem.balance.toString() }
+            tla={ elem.tla }
+            name={ elem.name }
+          />);
+      }) }
     </div>);
   }
 }
 
-class CoinIcon extends ReactiveComponent{
-  constructor(){
-    super(['balance','src','tla','name'])
+class CoinIcon extends ReactiveComponent {
+  constructor () {
+    super(['balance', 'src', 'tla', 'name']);
   }
 
   style = {
-	width: '40px',
-	height:'40px',
-	margin: '10px',
-	flexBasis: '25%'
+    width: '40px',
+    height: '40px',
+    margin: '10px',
+    flexBasis: '25%'
   }
 
-  render(){
+  render () {
     const { src, tla, name, balance } = this.state;
-    console.log('imgsrcs',src, unknownIcon);
-    return (<div style={{display: 'inline-block'}}>
+
+    console.log('imgsrcs', src, unknownIcon);
+    return (<div style={ { display: 'inline-block' } }>
       <Popup
-        key={tla}
-        trigger={<Image
-          style={this.style}
-          src={src || unknownIcon}
-	  alt=""
-	  >
-          </Image>}
-        header={name}
-        content={balance}
+        key={ tla }
+        trigger={
+          <Image
+            style={ this.style }
+            src={ src || unknownIcon }
+            alt=''
+          /> }
+        header={ name }
+        content={ balance }
       />
-    </div>);
+    </div>
+    );
   }
 }
