533b882429ee86256ebe4d2e08d5e6b97ae8a087
Arthur Stemmer <apstemmer@hotmail.com>
2017-11-24 11:19:19 +0000

    fix eslint webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,17 +3,17 @@ var path = require('path');
 
 module.exports = Object.assign(base, {
 	entry: {
-	app: path.join(__dirname, '..', '/test/manual/entry.jsx')
-},
-devServer: {
-	contentBase: path.join(__dirname, '..', 'public'),
-	compress: true,
-	port: 9999
-},
-output: {
-	path: path.resolve(__dirname, '..', 'public', 'dev'),
-	filename: 'bundle.js',
-	publicPath: '/dev',
-	libraryTarget: 'umd'
-},
+		app: path.join(__dirname, '..', '/test/manual/entry.jsx')
+	},
+	devServer: {
+		contentBase: path.join(__dirname, '..', 'public'),
+		compress: true,
+		port: 9999
+	},
+	output: {
+		path: path.resolve(__dirname, '..', 'public', 'dev'),
+		filename: 'bundle.js',
+		publicPath: '/dev',
+		libraryTarget: 'umd'
+	}
 });
