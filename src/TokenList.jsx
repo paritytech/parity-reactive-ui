05a4a70433026a5d049139b2ce5654aed5ac677d
Arthur Stemmer <apstemmer@hotmail.com>
2017-12-11 16:10:05 +0100

    Tokenlist into dropdown

--- src/TokenList.jsx
+++ src/TokenList.jsx
@@ -1,8 +1,8 @@
 const React = require('react');
 
 const { ReactiveComponent } = require('oo7-react');
-const { bonds } = require('oo7-parity');
-const { Popup, Image } = require('semantic-ui-react');
+const { bonds, formatToExponential } = require('oo7-parity');
+const { Popup, Image, Dropdown, Header } = require('semantic-ui-react');
 
 const unknownIcon = require('./assets/unknown-64x64.png');
 
@@ -19,29 +19,22 @@ class TokenList extends ReactiveComponent {
 		}
 
 		return (
-			<div
-				style={ {
-					display: 'inline-block',
-					backgroundColor: '#e2e2e2',
-					padding: '10px',
-					borderRadius: '10px',
-					overflow: 'hidden'
-				}	}
-			>
-				{ tokens.map((elem) => {
-					return (
-						<TokenIcon
-							key={ elem.name }
-							src={ bonds.githubhint.entries(elem.img).map((imgObj) => {
-								return imgObj[0];
-							}) }
-							balance={ elem.balance.toString() }
-							tla={ elem.tla }
-							name={ elem.name }
-						/>);
-				}) }
-
-			</div>
+			<Dropdown text='Tokens' floating labeled button icon='ticket' className='icon'>
+				<Dropdown.Menu>
+					{ tokens.map((elem) => {
+						return (
+							<TokenIcon
+								key={ elem.name }
+								src={ bonds.githubhint.entries(elem.img).map((imgObj) => {
+									return imgObj[0];
+								}) }
+								balance={ elem.balance.toString() }
+								tla={ elem.tla }
+								name={ elem.name }
+							/>);
+					}) }
+				</Dropdown.Menu>
+			</Dropdown>
 		);
 	}
 }
@@ -52,30 +45,19 @@ class TokenIcon extends ReactiveComponent {
 	}
 
 	render () {
-		const { src, tla, name, balance } = this.state;
+		let { src, tla, name, balance } = this.state;
 
+		balance = balance || 0;
+		console.log('bala', balance);
 		return (
-			<div style={ {
-				margin: '10px',
-				display: 'inline-block'
-			} }
-			>
-				<Popup
-					key={ tla }
-					trigger={
-						<Image
-							style={ {
-								width: '40px',
-								height: '40px',
-								flexBasis: '25%'
-							} }
-							src={ src || unknownIcon }
-							alt=''
-						/> }
-					header={ name }
-					content={ balance }
-				/>
-			</div>
+			<Dropdown.Item
+				content={
+					<Header
+						image={ src || unknownIcon }
+						content={ name }
+						subheader={ `${formatToExponential(balance || 0)} ${tla}` }
+					/> }
+			/>
 		);
 	}
 }
